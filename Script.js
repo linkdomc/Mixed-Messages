cdf9f257389d2d4654328d63b627dd23b5083cd5
linkdomc <linkdomc0@gmail.com>
2023-03-31 19:52:54 -0400

    works in code pen but must debug to figure out why it wont work in vscode

--- Script.js
+++ Script.js
@@ -1,11 +1,11 @@
-const verb = [`Running` , `Swiming` , `Sleeping`, `Working`];
-const sentence = [`The world may be ${word} but you should be ${word2}`, 
-                  `Never give up on ${word} you dream need you to be ${word2}`];
+const contener = document.querySelector("div");
 
-document.querySelector("div").onmouseover = event => {
-    let word = verb[Math.floot(Math.random()*verb.length-1)];
-    let word2 = verb[Math.floot(Math.random()*verb.length-1)];
-    let quote = sentence[Math.floot(Math.random()*sentence.length-1)];
+contener.onmouseover = event => {
+    const verb = [`Running` , `Swiming` , `Sleeping`, `Working`];
+    let word = verb[Math.floor(Math.random()*verb.length-1)];
+    let word2 = verb[Math.floor(Math.random()*verb.length-1)];
+    const sentence = [`The world may be ${word} but you should be ${word2}`, 
+                  `Never give up on ${word} you dream need you to be ${word2}`];
+    let quote = sentence[Math.floor(Math.random()*sentence.length-1)];
     event.target.innerText = quote;
-    console.log(quote);
 }
