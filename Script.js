055b5106b7905830de4294eaa43c6299d5424670
linkdomc <linkdomc0@gmail.com>
2023-03-31 23:08:14 -0400

    fixed undefine error

--- Script.js
+++ Script.js
@@ -2,10 +2,10 @@ const contener = document.querySelector("div");
 
 contener.onmouseover = event => {
     const verb = [`Running` , `Swiming` , `Sleeping`, `Working`];
-    let word = verb[Math.floor(Math.random()*verb.length-1)];
-    let word2 = verb[Math.floor(Math.random()*verb.length-1)];
+    let word = verb[Math.floor(Math.random()*verb.length)];
+    let word2 = verb[Math.floor(Math.random()*verb.length)];
     const sentence = [`The world may be ${word} but you should be ${word2}`, 
                   `Never give up on ${word} you dream need you to be ${word2}`];
-    let quote = sentence[Math.floor(Math.random()*sentence.length-1)];
+    let quote = sentence[Math.floor(Math.random()*sentence.length)];
     event.target.innerText = quote;
 }
