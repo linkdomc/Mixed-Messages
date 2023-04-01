d7151cab0fa1de23599c31772fc7e53d98db2098
linkdomc <linkdomc0@gmail.com>
2023-04-01 00:35:53 -0400

    Made the default text have a fade animation     and made the text creation on click and     not hover

--- Script.js
+++ Script.js
@@ -1,11 +1,21 @@
 const contener = document.querySelector("div");
+const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-contener.onmouseover = event => {
+
+onclick = event => {
+    let quote = create_Quote();
+        event.target.innerText = quote;
+}
+
+create_Quote = () =>{
     const verb = [`Running` , `Swiming` , `Sleeping`, `Working`];
+    
     let word = verb[Math.floor(Math.random()*verb.length)];
     let word2 = verb[Math.floor(Math.random()*verb.length)];
+    
     const sentence = [`The world may be ${word} but you should be ${word2}`, 
                   `Never give up on ${word} you dream need you to be ${word2}`];
-    let quote = sentence[Math.floor(Math.random()*sentence.length)];
-    event.target.innerText = quote;
+    
+    
+    return sentence[Math.floor(Math.random()*sentence.length)];
 }
